da0092a7eed72b1576de0d43f65438ba0582e1a9
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-08 18:56:58 +0530

    left nav bar done

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -2,10 +2,26 @@ import React, { useContext } from "react";
 import { categories } from "../utils/constant";
 import LeftNavMenuItems from "./LeftNavMenuItems";
 import { Context } from "../context/contextApi";
+import { useNavigate } from "react-router-dom";
 
 const LeftNav = () => {
   const { selectCategoroies, setSelectCategoroies, mobileMenu } =
     useContext(Context);
+
+  const navigate = useNavigate();
+
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case "category":
+        return setSelectCategoroies(name);
+      case "home":
+        return setSelectCategoroies(name);
+      case "menu":
+        return false;
+      default:
+        break;
+    }
+  };
   return (
     <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[240px] md:translate-x-0 transition-all ">
       <div className="flex px-5 flex-col h-screen">
@@ -13,10 +29,11 @@ const LeftNav = () => {
           return (
             <>
               <LeftNavMenuItems
-                text={item.type === "home" ? "home" : item.name}
+                text={item.type === "home" ? "Home" : item.name}
                 icon={item.icon}
                 action={() => {
-                  "";
+                  clickHandler(item.name, item.type);
+                  navigate("/");
                 }}
                 className={`${
                   selectCategoroies === item.name ? "bg-white/[0.15]" : ""
